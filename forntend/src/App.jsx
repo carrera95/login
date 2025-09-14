3be911e018690d9fd5e6cda0649b512041bdf177
FableV25 <sgtfablev@gmail.com>
2025-09-14 12:10:36 -0600

    full on port of the login app, (the previous repository is a dump)

---
+++ forntend/src/App.jsx
@@ -0,0 +1,59 @@
+import react from 'react'
+import { BrowserRouter, Routes, Route, Navigate} from 'react-router-dom'
+
+import Home from './pages/home'
+import Login from './pages/login'
+import Register from './pages/register'
+import NotFound from './pages/notFound'
+import ProtectedRoute from './components/protectedRoute'
+
+function LogOut() {
+  localStorage.clear()
+  return <Navigate to="/login" />
+}
+
+function RegisterAndLogOut() {
+  localStorage.clear()
+  return <Register />
+}
+
+function App() {
+  return (
+      <BrowserRouter> 
+        <Routes>
+          <Route
+            path="/"
+            element={
+              <ProtectedRoute>
+                <Home />
+              </ProtectedRoute>
+            }
+          />
+
+          <Route 
+            path='/login' 
+            element={<Login />}>
+          </Route>
+
+          <Route 
+            path='/logout' 
+            element={<LogOut />}>
+          </Route>
+
+
+          <Route 
+            path='/register' 
+            element={<RegisterAndLogOut />}>
+          </Route>
+
+          <Route 
+            path="*" 
+            element={<NotFound />}>
+          </Route>
+
+        </Routes> 
+      </BrowserRouter>
+  )
+}
+
+export default App
